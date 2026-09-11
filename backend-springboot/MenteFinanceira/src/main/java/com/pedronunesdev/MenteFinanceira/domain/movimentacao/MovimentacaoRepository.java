0b95510090b851cc8b35f6cd74ed04dcca35bd51
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-09-10 22:44:14 -0300

    feat: cria repositories das entidades atuais

---
+++ backend-springboot/MenteFinanceira/src/main/java/com/pedronunesdev/MenteFinanceira/domain/movimentacao/MovimentacaoRepository.java
@@ -0,0 +1,33 @@
+package com.pedronunesdev.MenteFinanceira.domain.movimentacao;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface MovimentacaoRepository extends JpaRepository<Movimentacao,Long> {
+
+    @Query("""
+    SELECT m FROM Movimentacao m
+    WHERE m.carteira.usuario.id = :id
+    ORDER BY dataDeExecucao DESC
+""")
+    Page<MovimentacaoDTOResponse> historicoMovimentacoes(@Param("id") Long idUsuario, Pageable pageable);
+
+    @Query("""
+    SELECT
+        m.categoriaMovimentacao,
+        SUM(m.valorMovimentado)
+    FROM Movimentacao m
+    WHERE m.carteira.usuario.id = :id
+    AND m.tipoMovimentacao = 'RETIRADA'
+    AND m.dataDeExecucao BETWEEN :diaPrimeiro AND :diaUltimo
+    GROUP BY m.categoriaMovimentacao
+    ORDER BY SUM(m.valorMovimentado) DESC
+""")
+    List<CategoriaTotalDTO> totalPorCategoria(@Param("id") Long idUsuario, @Param("diaPrimeiro") LocalDateTime diaPrimeiro, @Param("diaUltimo") LocalDateTime diaUltimo);
+}
---
+++ backend-springboot/MenteFinanceira/src/main/java/com/pedronunesdev/MenteFinanceira/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.pedronunesdev.MenteFinanceira.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+
+    Optional<Usuario> findByEmail(String email);
+}
