fc96da96e68ec907e462beb4dc4eaa9eeae03ec7
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-09-10 22:48:26 -0300

    feat: sobe main da api em spring

---
+++ backend-springboot/MenteFinanceira/src/main/java/com/pedronunesdev/MenteFinanceira/MenteFinanceiraApplication.java
@@ -0,0 +1,13 @@
+package com.pedronunesdev.MenteFinanceira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenteFinanceiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenteFinanceiraApplication.class, args);
+	}
+
+}
