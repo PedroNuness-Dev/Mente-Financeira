0841686db8fa1009cd22aad32a27a8ec4d1a4fb3
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-09-17 23:40:17 -0300

    docs: documenta o CategoriaMovimentacaoController.java

--- backend-springboot/MenteFinanceira/src/main/java/com/pedronunesdev/MenteFinanceira/controllers/movimentacao/CategoriaMovimentacaoController.java
+++ backend-springboot/MenteFinanceira/src/main/java/com/pedronunesdev/MenteFinanceira/controllers/movimentacao/CategoriaMovimentacaoController.java
@@ -2,6 +2,8 @@ package com.pedronunesdev.MenteFinanceira.controllers.movimentacao;
 
 import com.pedronunesdev.MenteFinanceira.enums.movimentacao.CategoriaMovimentacao;
 import com.pedronunesdev.MenteFinanceira.services.movimentacao.CategoriaMovimentacaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +15,13 @@ import java.util.List;
 @RestController
 @RequestMapping("api/movimentacao/categoria")
 @RequiredArgsConstructor
+@Tag(name = "Categoria Movimentacao Controller", description = "Responsável pelas ações relacioandas as categorias das movimentações")
 public class CategoriaMovimentacaoController {
 
     private final CategoriaMovimentacaoService categoriaMovimentacaoService;
 
+    @Operation(summary = "Busca todas as movimentações",
+            description = "Realiza a busca de todas as movimentações registradas no sistema")
     @GetMapping
     public ResponseEntity<List<CategoriaMovimentacao>> buscarTodasAsCategoriasDemovimentacao(){
 
