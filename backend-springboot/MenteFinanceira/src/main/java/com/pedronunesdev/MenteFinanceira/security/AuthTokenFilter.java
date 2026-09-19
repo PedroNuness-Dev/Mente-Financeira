e4932b2b2911e2f3dc4578798f11135964d125eb
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-09-19 17:08:26 -0300

    feat: cria filtro de autenticação

---
+++ backend-springboot/MenteFinanceira/src/main/java/com/pedronunesdev/MenteFinanceira/security/AuthTokenFilter.java
@@ -0,0 +1,50 @@
+package com.pedronunesdev.MenteFinanceira.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    private final JWTService jwtService;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String token = extrairToken(request);
+
+        if (token != null && jwtService.isJWTValido(token)){
+            String email = jwtService.validarJWTEObterSubject(token);
+            UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request,response);
+    }
+
+    private String extrairToken(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+
+        if (headerAuth != null && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7);
+        }
+
+        return null;
+    }
+}
