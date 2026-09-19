6abcdece58c71cc48a084ddbfa40d546589bfabd
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-09-19 17:11:13 -0300

    refactor: ajusta @JsonFormat para o LocalDateTime exibir corretamente

--- backend-springboot/MenteFinanceira/src/main/java/com/pedronunesdev/MenteFinanceira/exception/ExceptionResponse.java
+++ backend-springboot/MenteFinanceira/src/main/java/com/pedronunesdev/MenteFinanceira/exception/ExceptionResponse.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 
 public record ExceptionResponse(
-        @JsonFormat(pattern = "dd/MM/yyyy")
+        @JsonFormat(pattern = "dd/MM/yyyy HH:ss:mm")
         LocalDateTime timestamp,
         Integer status,
         String messagem,
