0c4c3433366755ab9a70fa3f1e384e421ad70ad8
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-09-25 07:00:39 -0300

    feat: cria método de busca do perfil do usuário

--- mente-financeira-angular/src/app/services/usuario/usuario-service.ts
+++ mente-financeira-angular/src/app/services/usuario/usuario-service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { tap } from 'rxjs';
 import { UsuarioRequest } from '../../interfaces/usuario/UsuarioRequest';
 import { UsuarioResponse } from '../../interfaces/usuario/UsuarioResponse';
 
@@ -7,11 +8,20 @@ import { UsuarioResponse } from '../../interfaces/usuario/UsuarioResponse';
   providedIn: 'root',
 })
 export class UsuarioService {
-
+  
   private httpClient = inject(HttpClient);
-  private url = 'http://localhost:8080'
+  private url = 'http://localhost:8080';
 
-  cadastrarUsuario(usuarioRequest : UsuarioRequest){
-    return this.httpClient.post<UsuarioResponse>(`${this.url}/api/usuarios/cadastro`, usuarioRequest);
+  private usuarioAtual = signal<UsuarioResponse | null>(null);
+  readonly usuario = this.usuarioAtual.asReadonly();
+
+  cadastrarUsuario(usuarioRequest: UsuarioRequest) {
+    return this.httpClient.post<UsuarioResponse>(`${this.url}/api/users/register`, usuarioRequest);
+  }
+
+  buscarPerfilDoUsuarioAutenticado() {
+    return this.httpClient
+      .get<UsuarioResponse>(`${this.url}/api/users/profile`)
+      .pipe(tap((res) => this.usuarioAtual.set(res)));
   }
 }
