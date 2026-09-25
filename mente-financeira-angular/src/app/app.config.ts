274c508032cab73640b49ff3ca7d0d24c158c2f8
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-09-25 07:00:17 -0300

    feat: adiciona o error-interceptor ao app.config

--- mente-financeira-angular/src/app/app.config.ts
+++ mente-financeira-angular/src/app/app.config.ts
@@ -4,11 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { authInterceptor } from './auth.interceptor';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorInterceptor } from './error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor,errorInterceptor])),
   ]
 };
